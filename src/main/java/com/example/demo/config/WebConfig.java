e014cf6f6275bb1b84e4e8a447d3a6cdcaaee72f
Jhan-Jhan17 <24-03424@g.batstate-u.edu.ph>
2026-05-17 12:55:32 +0800

    Backup before renaming project to System 1

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -12,12 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Find the exact absolute path to the uploads folder on the server
+        // This tells Spring Boot to expose the "uploads" folder to the web browser
         Path uploadDir = Paths.get("uploads");
         String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-        // Grant permission to serve these files to the internet
+        
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath + "/");
+                .addResourceLocations("file:/" + uploadPath + "/");
     }
 }
